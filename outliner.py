673d49ef77e876be6fd85f82c90065069da81d08
Vishwaak <chandranvishwaak@gmail.com>
2020-03-19 00:59:53 +0530

    Human detection HOG

--- outliner.py
+++ outliner.py
@@ -1,18 +1,24 @@
 import cv2
 import numpy as np
 
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+cv2.startWindowThread()
 cap = cv2.VideoCapture(0)
-
+out = cv2.VideoWriter('output.avi' , cv2.VideoWriter_fourcc(*'MJPG'),15.,(640,480))
 while True:
     _, frame = cap.read()
-    hsv = cv2.cvtColor(frame , cv2.COLOR_BGR2HSV)
-    lower_blue = np.array([38,86,0])
-    upper_blue = np.array([121,255,255])
-    mask = cv2.inRange(hsv , lower_blue , upper_blue)
-    cv2.imshow("Frame" , frame)
-    cv2.imshow("Mask" , mask)
-    key = cv2.waitKey(1)
-    if key == 27:
+    frame = cv2.resize(frame , (640,480))
+    gray = cv2.cvtColor(frame , cv2.COLOR_RGB2GRAY)
+    boxes , weights = hog.detectMultiScale(frame ,winStride=(8,8))
+    boxes = np.array([[x,y,x+w,y+h] for (x,y,w,h) in boxes])
+    for (xA,yA,xB,yB) in boxes:
+        cv2.rectangle(frame,(xA,yA),(xB,yB),(0,255,0),2)
+    out.write(frame.astype('uint8'))
+    cv2.imshow('frame' , frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
+out.release()
 cv2.destroyAllWindows()
+cv2.waitKey(1)
