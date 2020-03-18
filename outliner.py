d3a8838aec26219daad3e65b2e135d2a5320cb74
Vishwaak <chandranvishwaak@gmail.com>
2020-03-18 23:15:50 +0530

    masks

--- outliner.py
+++ outliner.py
@@ -1,11 +1,16 @@
 import cv2
-import numpy
+import numpy as np
 
 cap = cv2.VideoCapture(0)
 
 while True:
     _, frame = cap.read()
+    hsv = cv2.cvtColor(frame , cv2.COLOR_BGR2HSV)
+    lower_blue = np.array([38,86,0])
+    upper_blue = np.array([121,255,255])
+    mask = cv2.inRange(hsv , lower_blue , upper_blue)
     cv2.imshow("Frame" , frame)
+    cv2.imshow("Mask" , mask)
     key = cv2.waitKey(1)
     if key == 27:
         break
